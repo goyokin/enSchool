65a175d1a7adbd5ced297347ecf033116fec943b
goyokin <goyokin@gmail.com>
2014-04-18 17:33:47 -0700

    intergare with new server

--- jsb/jsblib/src/com/jsb/chat/AccountManager.java
+++ jsb/jsblib/src/com/jsb/chat/AccountManager.java
@@ -1,7 +1,17 @@
 package com.jsb.chat;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+
 import org.jivesoftware.smack.Connection;
 import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smackx.Form;
+import org.jivesoftware.smackx.ReportedData;
+import org.jivesoftware.smackx.ReportedData.Row;
+import org.jivesoftware.smackx.ServiceDiscoveryManager;
+import org.jivesoftware.smackx.search.UserSearchManager;
 
 import android.content.Context;
 
@@ -100,25 +110,69 @@ public class AccountManager {
 			mStatus = STAT_CONNECTED;
 		}
 		
+		String tmp = null;
 		try {
-			mCurrentConn.login(username, password);
+			tmp = username.substring(0, username.indexOf('@'));
 		} catch (Exception e) {
-			Tracer.e(TAG, "fail to login");
+			Tracer.e(TAG, "fail to parse the uesr name", e);
+			return false;
+		}
+
+		try {
+			mCurrentConn.login(tmp, password);
+		} catch (Exception e) {
+			Tracer.e(TAG, "fail to login", e);
 			mStatus = STAT_CONNECTED;
 			return false;
 		}
 		mStatus = STAT_LOGGED_IN;
+		Tracer.i(TAG, "user " + tmp + " logged in");
 		
 		mFullUserName = username;
 		mPassword = password;
-		try {
-			mUserName = username.substring(0, username.indexOf('@'));
-		} catch (Exception e) {
-			Tracer.w(TAG, "fail to parse the uesr name");
-		}
+		mUserName = tmp;
 		
 		return true;
 	}
+	
+	// Test code for registration
+	/* public boolean newUser() {
+		if (mCurrentConn == null)  
+            return false;
+        Registration reg = new Registration();  
+        reg.setType(IQ.Type.SET);  
+        reg.setTo(getConnection().getServiceName());  
+        // no jid  
+        reg.setUsername(account);  
+        reg.setPassword(password);  
+        // addAttribute cannot be null.  
+        reg.addAttribute("android", "geolo_createUser_android");  
+        PacketFilter filter = new AndFilter(new PacketIDFilter(  
+                reg.getPacketID()), new PacketTypeFilter(IQ.class));  
+        PacketCollector collector = getConnection().createPacketCollector(  
+                filter);  
+        getConnection().sendPacket(reg);  
+        IQ result = (IQ) collector.nextResult(SmackConfiguration  
+                .getPacketReplyTimeout());    
+        collector.cancel();  
+        if (result == null) {  
+            Log.e("regist", "No response from server.");  
+            return "0";  
+        } else if (result.getType() == IQ.Type.RESULT) {  
+            Log.v("regist", "regist success.");  
+            return "1";  
+        } else { // if (result.getType() == IQ.Type.ERROR)  
+            if (result.getError().toString().equalsIgnoreCase("conflict(409)")) {  
+                Log.e("regist", "IQ.Type.ERROR: "  
+                        + result.getError().toString());  
+                return "2";  
+            } else {  
+                Log.e("regist", "IQ.Type.ERROR: "  
+                        + result.getError().toString());  
+                return "3";  
+            }  
+        }  
+	} */
 
 	public boolean isUserConnected(String username) {
 		return (mStatus == STAT_LOGGED_IN && username.compareTo(mFullUserName) == 0);
@@ -151,4 +205,46 @@ public class AccountManager {
 	public String getResource() {
 		return mResource;
 	}
+	
+	public boolean deleteAccount() {  
+        try {
+            mCurrentConn.getAccountManager().deleteAccount();
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+	
+	public List<HashMap<String, String>> searchUsers(String userName) {  
+        HashMap<String, String> user = null;
+        List<HashMap<String, String>> results = new ArrayList<HashMap<String, String>>();  
+        try {  
+            new ServiceDiscoveryManager(getConnection());  
+  
+            UserSearchManager usm = new UserSearchManager(getConnection());  
+  
+            Form searchForm = usm.getSearchForm(getConnection()  
+                    .getServiceName());  
+            Form answerForm = searchForm.createAnswerForm();  
+            answerForm.setAnswer("userAccount", true);  
+            answerForm.setAnswer("userPhote", userName);  
+            ReportedData data = usm.getSearchResults(answerForm, "search"  
+                    + getConnection().getServiceName());  
+  
+            Iterator<Row> it = data.getRows();  
+            Row row = null;  
+            while (it.hasNext()) {  
+                user = new HashMap<String, String>();  
+                row = it.next();  
+                user.put("userAccount", row.getValues("userAccount").next()  
+                        .toString());  
+                user.put("userPhote", row.getValues("userPhote").next()  
+                        .toString());  
+                results.add(user);  
+            }  
+        } catch (Exception e) {  
+            Tracer.e(TAG, "fail to find user");  
+        }  
+        return results;  
+    }
 }
